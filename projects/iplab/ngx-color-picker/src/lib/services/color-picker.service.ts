620cd9fbe37ca4ddfd5825124c6f1f8991b7a91a
pIvan <pintar.ivan@gmail.com>
2020-02-26 12:30:24 +0100

    dependency update

--- projects/iplab/ngx-color-picker/src/lib/services/color-picker.service.ts
+++ projects/iplab/ngx-color-picker/src/lib/services/color-picker.service.ts
@@ -1,10 +1,12 @@
 import { Class } from 'estree';
+import { Injectable } from "@angular/core";
 
 export interface IColorPickerConfig {
     indicatorTitle: string;
     presetsTitle: string;
 }
 
+@Injectable()
 export class ColorPickerConfig implements IColorPickerConfig {
     public indicatorTitle: string = 'Copy color to clipboard';
     public presetsTitle: string = '{0}. Long-click to show alternate shades.';
